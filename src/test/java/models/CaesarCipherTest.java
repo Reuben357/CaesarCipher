7be320b631fc6d6c83e3ca6020c68df628f2be09
Reuben <gathiireuben@gmail.com>
2020-04-27 21:34:23 +0300

    adds tests

--- src/test/java/models/CaesarCipherTest.java
+++ src/test/java/models/CaesarCipherTest.java
@@ -1,5 +1,40 @@
 package models;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class CaesarCipherTest {
+
+    @Test
+    public void testForConstructor() throws Exception {
+        Cipher cipherObject = new Cipher("Sample secret message", 5);
+        // assert that cipherObject is an instance of Cipher, true or false
+        assertEquals(true, cipherObject instanceof Cipher);
+    }
+
+    @Test
+    public void testInputMessage() throws Exception{
+        Cipher cipherObject = new Cipher("Sample secret message", 5);
+        assertEquals("Sample secret message", cipherObject.getMessage());
+    }
+
+    @Test
+    // GIDEON maroko key - 7 NPKLVU thyvrv
+    public void testCipherLogic() throws Exception {
+        Cipher cipherObject = new Cipher("GIDEON maroko", 7);
+        assertEquals("NPKLVU thyvrv", cipherObject.messageInput());
+    }
+    @Test
+    // GIDEON maroko key - 7 NPKLVU thyvrv
+    public void testShiftKeyLogic() throws Exception {
+        Cipher cipherObject = new Cipher("GIDEON maroko", 7);
+        assertEquals(7, cipherObject.getShiftKey());
+    }
+
+    @Test //UWRSCB aofcyc
+    public void testDecipherogic() throws Exception{
+        Decipher decipherObject = new Decipher("NPKLVU thyvrv", 7);
+        assertEquals("GIDEON maroko", decipherObject.decipherUserText());
+    }
+
 
 }
